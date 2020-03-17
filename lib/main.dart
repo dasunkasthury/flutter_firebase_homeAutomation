91970c9780b0e4beb24758fb21e1b4daf652e9fc
Dasun Kasthuri <dasun.k@eyepax.com>
2020-03-17 23:42:39 +0530

    before going to use firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,110 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:english_words/english_words.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+import './question.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HomeAutomationApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomeAutomationApp extends StatefulWidget {
+  @override
+  _HomeAutomationAppState createState() => _HomeAutomationAppState();
+}
+
+class _HomeAutomationAppState extends State<HomeAutomationApp> {
+  var size1 = 100.1;
+  int questionNumber = 0;
+
+  List<Icon> scoreKeeper = [];
+
+  void playSound(int soundIndex) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+    assetsAudioPlayer.open(
+      "assets/note$soundIndex.wav",
+    );
+    assetsAudioPlayer.play();
+  }
+
+  FlatButton soundKey(int soundNumber, Color color) {
+    return FlatButton(
+      onPressed: () {
+        playSound(soundNumber);
+      },
+      child: Container(
+        height: 20,
+        width: 100,
+        color: color,
+      ),
+    );
+  }
+
+  List<Question> qList = [
+    new Question(q: 'aa', a: true),
+    new Question(q: 'bb', a: false),
+    new Question(q: 'cc', a: true)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Home Automation App"),
+          ),
+          backgroundColor: Colors.indigo,
         ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Image(
+                  image: AssetImage('images/robot.png'),
+                  height: 200,
+                  width: 150,
+                ),
+                Text(nouns.first),
+                SizedBox(
+                  height: 25,
+                ),
+                Expanded(
+                    child: Column(
+                  children: <Widget>[
+                    soundKey(1, Colors.green),
+                    soundKey(2, Colors.red),
+                    soundKey(3, Colors.blue),
+                    Row(
+                      children: scoreKeeper,
+                    ),
+                    Text(qList[questionNumber].questionText),
+                    FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          if (questionNumber < 2) {
+                            questionNumber++;
+                          }
+                        });
+
+                        if (qList[questionNumber].questionAnswer == true) {
+                          setState(() {
+                            scoreKeeper.add(Icon(
+                              Icons.check,
+                              color: Colors.white,
+                            ));
+                          });
+                        }
+                      },
+                      child: Text("TRUE"),
+                      color: Colors.purple,
+                    )
+                  ],
+                ))
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.lime,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
